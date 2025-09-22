c06bdaf3c41977df9d89d8bdd70f96b939264918
Bartek Zaborowski <b.zaborowski.bz@gmail.com>
2025-09-22 22:12:53 +0200

    Still work on the buttons

--- Cat/index.js
+++ Cat/index.js
@@ -1,19 +1,34 @@
 
 const addBtn = document.getElementById("add-btn");
+const addBtnTwo = document.getElementById("add-btn-two");
+
 const dataOne = document.getElementById("data-one")
-const newP = document.createElement("p")
+const dataTwo = document.getElementById("data-two")
+
 const addData = document.getElementById("add")
+const addDataTwo = document.getElementById("add-two")
 
 
-
+"add-btn-two"
 
 addBtn.addEventListener('click', function() {
     addFood();
 });
+addBtnTwo.addEventListener('click', function() {
+    addFoodTwo();
+});
+
 
 function addFood() {
     const value = addData.value;
     const newP = document.createElement("p");    
     newP.textContent = value;
     dataOne.append(newP);
+}
+
+function addFoodTwo(){    
+    const value = addDataTwo.value;
+    const newP = document.createElement("p");    
+    newP.textContent = value;
+    dataTwo.append(newP);
 }
